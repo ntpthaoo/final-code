12754459c12ae3fd4338ea3412ce35e25d950924
Ubuntu <ubuntu@ip-172-31-87-7.ec2.internal>
2023-05-07 13:53:26 +0000

    hihi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">Login (New version)</div>
 
                     @include('components.flash-message')
 
